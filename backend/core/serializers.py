4fb3a1257d1247dbe6ab65f9bab47b633f970349
Abhishek Kumar <abhishek1py@gmail.com>
2025-06-07 16:10:14 +0530

    serializers.py: Validators > Field Level Validation > Object Level Validation

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,17 +1,50 @@
 from rest_framework import serializers
 from .models import Account
 from datetime import datetime
+import re
+
+
+# Priority:- Validators > Field Level Validation > Object Level Validation
+# 1. Validators : Validate name: only letters (a-z, A-Z) and spaces
+# This single validators can be used in multiple fields and in one field we can use multiple validators.
+def no_special_chars(value):
+        if not re.fullmatch(r"[A-Za-z ]+", value):
+            raise serializers.ValidationError("Name can contain only alphabets and spaces")
+        return value
 
 class AccountSerializer(serializers.Serializer):
     # id = serializers.CharField(max_length=50, read_only=True) # For UUID
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
+    name = serializers.CharField(max_length=100, validators=[no_special_chars])
     description = serializers.CharField(max_length=100)
     bank = serializers.ChoiceField(choices=Account.BANK_CHOICES)
     account_number = serializers.CharField(max_length=20)
     balance = serializers.DecimalField(max_digits=15, decimal_places=2, default=0.00)
     created_at = serializers.DateTimeField(read_only=True)
 
+    # 2. Field Level Validation : When you have one field to validate.
+    # def validate_balance(self, value):
+    #     if value < 0:
+    #         raise serializers.ValidationError("Amount balance must be positive.")
+    #     return value
+
+    # 3. Object Level Validation : When you have more fields to validate.
+    # def validate(self, data):
+    #     balance = data.get('balance')
+    #     name = data.get('name')
+    #     print(f"balance : {balance}")
+    #     print(f"name : {name}")
+
+    #     # Validate balance
+    #     if balance < 0:
+    #         raise serializers.ValidationError(
+    #             "Amount balance must be positive.")
+
+    #     # Validate name: only letters (a-z, A-Z) and spaces
+    #     if not re.fullmatch(r"[A-Za-z ]+", name):
+    #         raise serializers.ValidationError("Name can contain only alphabets and spaces")
+
+    #     return data
 
     # Deserialization
     def create(self, validated_data):
