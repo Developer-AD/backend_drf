191a874a13a1330d23139c786754fd0c66e10f1f
Abhishek Kumar <abhishek1py@gmail.com>
2025-06-07 11:21:27 +0530

    Added docs in serializers.py - Serializer Fields and Core Arguments

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -2,6 +2,50 @@ from rest_framework import serializers
 from .models import Account
 from datetime import datetime
 
+""" -------------- Serializer Fields -----------------.
+name =  serializers.CharField(min_length=3, 
+        max_length=5, 
+        allow_null=True,  # Instead of allow_null use `allow_blank`
+        allow_blank=True, 
+        trim_whitespace=True)
+
+UUIDField()
+IntegerField()
+FloatField()
+DecimalField()
+SlugField()
+EmailField()
+BooleanField()
+URLField()
+FileField()
+FilePathField()
+ImageField()
+DateField()
+TimeField()
+DateTimeField()
+DurationField()
+IPAddressField()
+ChoiceField()
+MultipleChoiceField()
+ListField()
+DictField()
+JSONField()
+"""
+
+""" -------------- Core Arguments --------------
+
+-label
+-style
+-validators
+-error_messages
+-help_text
+-required
+-default
+-read_only=False [default]
+-write_only=True [default]
+-allow_null
+"""
+
 
 class AccountSerializer(serializers.Serializer):
     # id = serializers.CharField(max_length=50, read_only=True) # For UUID
