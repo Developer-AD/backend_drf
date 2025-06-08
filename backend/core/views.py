a18ec79384f928da17f9857f0d3c28256eb75a0f
Abhishek Kumar <abhishek1py@gmail.com>
2025-06-08 17:07:03 +0530

    Authentication using TokenAuthentication - Generate Token via Admin, drf_create_token and api-token-auth

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,62 +1,19 @@
 from .models import Account
 from .serializers import AccountSerializer
-from rest_framework import status
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-
-
+from rest_framework import viewsets
+# from rest_framework.authentication import BasicAuthentication
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
-@api_view(['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
-@authentication_classes([BasicAuthentication])
-@permission_classes([IsAuthenticated, IsAdminUser])
-def account_view(request, pk=None):
-    if request.method == 'GET':
-        if pk is not None:
-            account = get_object_or_404(Account, id=pk)
-            serializer = AccountSerializer(account)
-            return Response(serializer.data)
-        
-        accounts = Account.objects.all()
-        serializer = AccountSerializer(accounts, many=True)
-        return Response(serializer.data)
+"""
+# from rest_framework.authentication import SessionAuthentication
+# rest_frameworks browsable api uses Session Authentication
+# Basic authentication/logout may be not worked.
+"""
 
-    if request.method == 'POST':
-        if pk is None:
-            serializer = AccountSerializer(data = request.data)
-            if serializer.is_valid():
-                serializer.save()
-                res = {"success":True, "message":"Account added successfully."}
-                return Response(res, status=status.HTTP_201_CREATED)
-
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    if request.method == 'PATCH':
-        if pk is not None:
-            account = get_object_or_404(Account, id=pk)
-            serializer = AccountSerializer(account, data = request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                res = {"success":True, "message":"Account partially updated successfully."}
-                return Response(res)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    if request.method == 'PUT':
-        if pk is not None:
-            account = get_object_or_404(Account, id=pk)
-            serializer = AccountSerializer(account, data = request.data)
-            if serializer.is_valid():
-                serializer.save()
-                res = {"success":True, "message":"Account fully updated successfully."}
-                return Response(res)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    if request.method == 'DELETE':
-        if pk is not None:
-            account = get_object_or_404(Account, id=pk)
-            account.delete()
-            res = {"success":True, "message":"Account deleted successfully."}
-            return Response(res)
+class AccountViewsets(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
