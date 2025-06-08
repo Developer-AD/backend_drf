9a2a039fbc3b5f1f43a6874fc4121706eba38e76
Abhishek Kumar <abhishek1py@gmail.com>
2025-06-08 10:30:57 +0530

    SessionAuthentication with DRF's browsable auth API

--- backend/core/views.py
+++ backend/core/views.py
@@ -2,16 +2,21 @@ from .models import Account
 from .serializers import AccountSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
+# from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
-
-
 # Create your views here.
-# You can use anythings either long fn to create views or 3 lines
-# Authentication will work same as it is.
+
+"""
+# from rest_framework.authentication import SessionAuthentication
+# rest_frameworks browsable api uses Session Authentication
+# Basic authentication/logout may be not worked.
+"""
+
 class AccountViewsets(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
-    # authentication_classes = [BasicAuthentication]
     # permission_classes = [IsAuthenticatedOrReadOnly]
-    # permission_classes = [IsAuthenticated]
+    # authentication_classes = [SessionAuthentication]
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAdminUser]
     # permission_classes = [AllowAny] # Same as without login.
