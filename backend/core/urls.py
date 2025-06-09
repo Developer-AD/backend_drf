a357418c0a8712d1e56d83be934035135ce9cdd3
Abhishek Kumar <abhishek1py@gmail.com>
2025-06-09 22:14:44 +0530

    Arranged api urls in project urls to token and auth

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,7 +2,6 @@
 from core import views
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 router = DefaultRouter()
 router.register(r'accounts', views.AccountViewsets, basename='account')
@@ -10,8 +9,4 @@ router.register(r'accounts', views.AccountViewsets, basename='account')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('rest_framework.urls')),
-    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify', TokenVerifyView.as_view(), name='token_verify'),
 ]
