bf42afd0aa0e76d73fd08c5e1eb9f06ac698c8f2
Abhishek Kumar <abhishek1py@gmail.com>
2025-06-09 21:54:04 +0530

    Fetch and Add account data Userwise using ModelViewset

--- backend/core/views.py
+++ backend/core/views.py
@@ -13,3 +13,9 @@ class AccountViewsets(viewsets.ModelViewSet):
     serializer_class = AccountSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Account.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
