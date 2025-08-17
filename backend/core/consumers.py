78fd6be54fdbc9fd9d0c0ce8d2978c1830ecea2d
Abhishek Kumar <abhishek1py@gmail.com>
2025-08-17 12:44:13 +0530

    Added Sync and Async consumer

--- backend/core/consumers.py
+++ backend/core/consumers.py
@@ -1,26 +1,110 @@
-import json
+from channels.consumer import SyncConsumer, AsyncConsumer
+# from channels.exceptions import StopConsumer
 
-from channels.generic.websocket import WebsocketConsumer
 
-
-class MySyncConsumer(WebsocketConsumer):
-    def connect(self):
+class MySyncConsumer(SyncConsumer):
+    def websocket_connect(self, event):
         print('-'*100)
         print('WEBSOCKET - CONNECT')
         print('-'*100)
-        self.accept()
+        self.send({
+            "type": "websocket.accept",
+        })
 
-    def disconnect(self, close_code):
+
+    def websocket_receive(self, event):
+        print('-'*100)
+        print('WEBSOCKET - CONNECT')
+        print(f"Event - Data : {event}")
+        print('-'*100)
+        
+        # text_data_json = json.loads(text_data)
+        # name = text_data_json.get("name")
+        message = f"Hello {event.get('text')}, Good morning."
+
+        print('-'*100)
+        self.send({
+            "type": "websocket.send",
+            "text": message,
+            # "text": event["text"],
+        })
+
+    def websocket_disconnect(self, close_code):
         print('*'*100)
         print('WEBSOCKET - DISCONNECT')
         print('*'*100)
         pass
 
-    def receive(self, text_data):
+# ---------------------------- ASYNC CONSUMER ---------------------------.
+class MyAsyncConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
         print('-'*100)
-        print('WEBSOCKET - CONNECT')
+        print('Async - WEBSOCKET - CONNECT')
         print('-'*100)
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        await self.send({
+            "type": "websocket.accept",
+        })
 
-        self.send(text_data=json.dumps({"message": message}))
+
+    async def websocket_receive(self, event):
+        print('-'*100)
+        print('Async - WEBSOCKET - CONNECT')
+        print(f"Event - Data : {event}")
+        print('-'*100)
+        
+        # text_data_json = json.loads(text_data)
+        # name = text_data_json.get("name")
+        message = f"Hello {event.get('text')}, Good morning."
+
+        print('-'*100)
+        await self.send({
+            "type": "websocket.send",
+            "text": message,
+            # "text": event["text"],
+        })
+
+    async def websocket_disconnect(self, close_code):
+        print('*'*100)
+        print('Async - WEBSOCKET - DISCONNECT')
+        print('*'*100)
+        pass
+
+
+# ===============================================================
+# import json
+
+# from channels.generic.websocket import WebsocketConsumer
+# from channels.exceptions import 
+
+
+# class MySyncConsumer(WebsocketConsumer):
+#     def connect(self):
+#         print('-'*100)
+#         print('WEBSOCKET - CONNECT')
+#         print('-'*100)
+#         self.accept()
+
+#     def disconnect(self, close_code):
+#         print('*'*100)
+#         print('WEBSOCKET - DISCONNECT')
+#         print('*'*100)
+#         pass
+
+#     def receive(self, text_data):
+#         print('-'*100)
+#         print('WEBSOCKET - CONNECT')
+#         print(f"Text - Data : {text_data}")
+#         print('-'*100)
+        
+#         # text_data_json = json.loads(text_data)
+#         # name = text_data_json.get("name")
+#         message = f"Hello {text_data}, Good morning."
+
+#         # if name:
+#         #     message = f"Hello {name}, Good morning."
+#         # else:
+#         #     message = f"Hello Annonymous, Good morning."
+#         # message = text_data_json["message"]
+
+#         print('-'*100)
+#         self.send(text_data=json.dumps({"message": message}))
--- backend/core/routing.py
+++ backend/core/routing.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
-from .consumers import MySyncConsumer
+from .consumers import MySyncConsumer, MyAsyncConsumer
 
 websocket_urlpatterns = [
     path('ws/sc/', MySyncConsumer.as_asgi()),
+    path('ws/ac/', MyAsyncConsumer.as_asgi()),
     # re_path(r"ws/sc/(?P<name>\w+)/$", MySyncConsumer.as_asgi()),
     # path('ws/ac/', MyAsyncConsumer.as_asgi()),
 ]
